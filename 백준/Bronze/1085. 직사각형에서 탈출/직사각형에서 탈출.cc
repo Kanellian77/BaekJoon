6f69df21ce26ac5627e6972a92c9918e6be12a2b
Kanellian77 <168343940+Kanellian77@users.noreply.github.com>
2024-09-06 23:26:01 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -1,31 +1,5 @@
-#include <string>
-#include <vector>
-#include <set>
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <map>
-#include <queue>
-#include <stack>
-#include <string.h>
+#include <bits/stdc++.h>
 
-#define infinity 10000000
-#pragma warning(disable:4996)
-using namespace std;
-typedef long long ll;
-
-int main()
-{
-	int x, y;
-	int w, h;
-	scanf("%d %d %d %d", &x, &y, &w, &h);
-
-	int xmin, ymin;
-	// w/2보다 큰지 아닌지 확인
-	xmin = (x > w / 2) ? w - x : x;
-	ymin = (y > h / 2) ? h - y : y;
-	int ans = min(xmin, ymin);
-	printf("%d", ans);
-
-	return 0;
-}
+int main(){	
+    int x, y, w, h;	scanf("%d %d %d %d", &x, &y, &w, &h); 
+    printf("%d", std::min(std::min(x, y), std::min(w - x, h - y)));}
