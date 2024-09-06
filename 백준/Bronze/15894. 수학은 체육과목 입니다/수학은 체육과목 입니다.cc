6c66c03a36e0a5fc10df545f846c42b4861b53e7
Kanellian77 <168343940+Kanellian77@users.noreply.github.com>
2024-09-06 23:42:22 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string.h>
+
+#define infinity 10000000
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	ll n;
+	scanf("%lld", &n);
+	printf("%lld", n * 4);
+	return 0;
+}
